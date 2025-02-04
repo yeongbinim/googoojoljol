b0a622e6fbf8dd0cd87b41610ca71b99095cf3bb
yeongbinim <yeim.dev@gmail.com>
2025-02-04 18:54:35 +0900

    ✨feat: synchronized로 동시성 해결

---
+++ src/main/java/googoo/joljol/shopping_mall/service/ShoppingMallLockService.java
@@ -0,0 +1,32 @@
+package googoo.joljol.shopping_mall.service;
+
+import static googoo.joljol.common.exception.ExceptionType.SHOPPING_MALL_NOT_FOUND;
+
+import googoo.joljol.common.exception.CustomException;
+import googoo.joljol.shopping_mall.entity.ShoppingMall;
+import googoo.joljol.shopping_mall.entity.ShoppingMallStats;
+import googoo.joljol.shopping_mall.repository.ShoppingMallRepository;
+import googoo.joljol.shopping_mall.repository.ShoppingMallStatsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ShoppingMallLockService {
+
+    private final ShoppingMallRepository shoppingMallRepository;
+    private final ShoppingMallStatsRepository shoppingMallStatsRepository;
+
+    public synchronized ShoppingMall getShoppingMallByIdWithSynchronized(Long id) {
+        ShoppingMall shoppingMall = shoppingMallRepository.findById(id)
+            .orElseThrow(() -> new CustomException(SHOPPING_MALL_NOT_FOUND));
+
+        ShoppingMallStats stats = shoppingMallStatsRepository.findByShoppingMallId(id)
+            .orElseThrow(() -> new CustomException(SHOPPING_MALL_NOT_FOUND));
+
+        stats.incrementViewCount();
+        shoppingMallStatsRepository.save(stats);
+
+        return shoppingMall;
+    }
+}
